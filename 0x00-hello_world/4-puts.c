9d8669ca9f6e0277ee1994f443dc7084d450b9ab
Iyk25 <ikeagyepong@yahoo.com>
2023-02-16 14:57:13 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main-Prints string using puts function
+ *Return:0
+*/
+
+int main(void)
+{
+
+puts("Programming is like building a multilingual puzzle);
+
+return (0);
+
+}
